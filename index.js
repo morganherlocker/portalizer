81246c90b4931c906e16c6d9555cb095632a505d
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-02-18 09:01:13 -0500

    portals scraping for ML training

--- index.js
+++ index.js
@@ -3,29 +3,44 @@ var _ = require('lodash'),
     csv = require('csv'),
     fs = require('fs'),
     ddg = require('ddg'),
-    classifier = require('classifier')
+    classifier = require('classifier'),
+    google = require('google');
+
+var portals = '"Location", "Link", "Title", "Description"',
+    locationFile = './statesTest.csv',
+    outputFile = './portals.csv'
 
 console.log('Gathering Locations')
 csv()
-.from.path('./states.csv')
-.to.array( function(states){
-  async.eachSeries(states,
-    function(state, cb){
+.from.path(locationFile)
+.to.array(function(locations){
+  async.eachSeries(locations,
+    function(location, cb){
       console.log('=================')
-      console.log('State: '+state[0])
-      getPortal(state[0], cb)
+      console.log('Location: '+location[0])
+      getPortal(location[0], cb)
     },
     function(){
+      fs.writeFileSync(outputFile, portals)
       console.log('Complete')
     }
   )
 })
 
 function getPortal(name, done){
-  console.log(name)
-  ddg.query(name + ' data portal', function(err, data){
-    console.log(data)
+  google.resultsPerPage = 10;
+  var nextCounter = 0;
 
+  google(name + ' data portal', function(err, next, links){
+    if (err) console.error(err);
+    for (var i = 0; i < links.length; ++i) {
+      console.log(links[i].title + ' - ' + links[i].link); //link.href is an alias for link.link
+      console.log(links[i].description + "\n");
+      portals += '\n"'+name+'",'
+      portals += '"'+links[i].link+'",'
+      portals += '"'+links[i].title+'",'
+      portals += '"'+links[i].description+'"'
+    }
     done()
   });
 }
